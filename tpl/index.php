44c6b79f2af24455610e9a4dc86c97d37b696124
izawasoma <izawasoma@gmail.com>
2021-12-19 03:40:15 +0900

    開発開始

---
+++ tpl/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gitの練習</title>
+</head>
+<body>
+    <h1>Gitの練習を行います</h1>
+    <p>---------------------------------------</p>
+    <h2>足し算を行う</h2>
+    <p>5 + 6 = 11</p>
+</body>
+</html>
