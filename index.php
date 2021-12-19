2b7547563b282a914a72a02729f63ece272694a2
izawasoma <izawasoma@gmail.com>
2021-12-19 12:25:48 +0900

    複数の値に対して計算処理を行えるように

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once './function/function.php';
-$num1 = 5;
-$num2 = 6;
+$num1 = [5,3,6,8,1,0,7,9,4,2];
+$num2 = [6,1,9,8,0,7,9,9,4,7];
 $add_ans = get_add($num1,$num2);
+require_once './tpl/index.php';
 ?>
